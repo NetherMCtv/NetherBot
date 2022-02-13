e2191ba19ac95aaa67f950556b2b4e57e508039c
NetherMC <thelio.trinite@outlook.com>
2022-02-13 12:45:19 +0100

    events

---
+++ src/events/ready.js
@@ -0,0 +1,18 @@
+module.exports = (client) => {
+  console.log(`Connecté en tant que ${client.user.tag}.`);
+
+  const statuses = [
+    ['sur Twitch', 'STREAMING'],
+    [`une vidéo de NetherMC`, 'WATCHING'],
+    ['Minecraft', 'PLAYING']
+  ];
+  
+  let i = 0;
+  setInterval(async () => {
+    await client.user.setActivity(statuses[i][0], {
+      type: statuses[i][1],
+      url: 'https://www.twitch.tv/NetherMCtv'
+    });
+    i = ++i % statuses.length;
+  }, 8e3);
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const {
   Client, Intents
 } = require('discord.js');
+const fs = require('fs');
 require('dotenv').config();
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS]
 });
 
-client.on('ready', () => {
-  console.log(`Connecté en tant que ${client.user.tag}`);
+fs.readdir(`${__dirname}/events`, (err, events) => {
+  if (err) {
+    console.error(err);
+  }
+
+  events.map(event => {
+    if (!event.endsWith('.js')) return;
+    const eventName = event.replace('.js', '');
+    const eventFunc = require(`${__dirname}/events/${event}`);
+
+    client.on(eventName, (...args) => eventFunc(...args, client));
+  });
 });
 
 client.login(process.env.BOT_TOKEN);
