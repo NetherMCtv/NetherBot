50afc2f41d979cfcba02596cdadca4c55a3b6a77
NetherMC <thelio.trinite@outlook.com>
2022-02-13 14:14:11 +0100

    events

--- src/index.js
+++ src/index.js
@@ -5,21 +5,28 @@ const fs = require('fs');
 require('dotenv').config();
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
 
-fs.readdir(`${__dirname}/events`, (err, events) => {
-  if (err) {
-    console.error(err);
-  }
+const eventsReadDir = (isWsEvent = false) => {
+  const path = isWsEvent ? `${__dirname}/events/ws` : `${__dirname}/events`;
+  fs.readdir(path, (err, events) => {
+    if (err) console.error(err);
 
-  events.map(event => {
-    if (!event.endsWith('.js')) return;
-    const eventName = event.replace('.js', '');
-    const eventFunc = require(`${__dirname}/events/${event}`);
+    events.map(event => {
+      if (!event.endsWith('.js')) return;
+      const eventName = event.replace('.js', '');
+      const eventFunc = require(`${path}/${event}`);
 
-    client.on(eventName, (...args) => eventFunc(...args, client));
+      isWsEvent ?
+        client.ws.on(eventName, (...args) => eventFunc(...args, client)) :
+        client.on(eventName, (...args) => eventFunc(...args, client));
+    });
   });
-});
+}
+// Load events
+eventsReadDir();
+// Load WS events
+eventsReadDir(true);
 
 client.login(process.env.BOT_TOKEN);
