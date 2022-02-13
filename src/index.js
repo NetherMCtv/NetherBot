db75b518c22f9d5c89e95cc84a8fb066deffc95d
NetherMC <thelio.trinite@outlook.com>
2022-02-13 12:40:04 +0100

    intents

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const {
-  Client
+  Client, Intents
 } = require('discord.js');
 require('dotenv').config();
 
-const client = new Client();
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS]
+});
 
 client.on('ready', () => {
   console.log(`Connecté en tant que ${client.user.tag}`);
