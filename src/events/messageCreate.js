62a4c3b10c770f35359f1af9d72c1f35e1e4cd10
NetherMC <thelio.trinite@outlook.com>
2022-02-13 14:13:28 +0100

    message event

---
+++ src/events/messageCreate.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+module.exports = (message, client) => {
+  console.log(message.content);
+  const prefix = '.';
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+
+  fs.readdir(`${__dirname}/../commands`, (err, commands) => {
+    if (err) {
+      console.error(err);
+    }
+
+    commands.map(file => {
+      if (!file.endsWith('.js')) return;
+      const commandName = file.replace('.js', '');
+      const commandFunc = require(`${__dirname}/../commands/${file}`);
+      if (command === commandName) {
+        (new commandFunc).run(message, client, args, true);
+      }
+    });
+  });
+}
