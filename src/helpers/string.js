03c7f290a3a34c9784c8f04d2dca4bcafaad82ab
NetherMC <thelio.trinite@outlook.com>
2022-02-13 18:40:55 +0100

    helpers

---
+++ src/helpers/string.js
@@ -0,0 +1,11 @@
+/**
+ * Emails
+ */
+
+module.exports.emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+module.exports.validateEmail = (email) => {
+  return email
+    .toLowerCase()
+    .match(this.emailRegex);
+};
