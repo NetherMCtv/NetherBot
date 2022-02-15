7a224f22b1561dabcc7686c328005929c940006d
NetherMC <thelio.trinite@outlook.com>
2022-02-15 21:39:14 +0100

    si mauvais channel

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -15,6 +15,20 @@ module.exports = (interaction, client) => {
       const commandFile = require(`${__dirname}/../commands/${file}`);
 
       if (command === commandName) {
+        if (
+          interaction.channelId !== '854063459259645962' && // #commandes
+          interaction.channelId !== '863805344622706709' && // #général-dev
+          interaction.channelId !== '942391531774562364' && // #tests-netherbot
+          !interaction.member.user.bot //|| // faut pas que ce soit un bot
+          //!interaction.author.roles.cache.get('863736992106151966') // si c'est pas un développeur
+        ) {
+          await interaction.reply({
+            content: `Allons **${interaction.member.user.username}**, fais-moi plaisir... Va exécuter cette commande dans <#854063459259645962> s'il-te-plait <:UwU:865554940334964757>`,
+            ephemeral: true
+          })
+          return;
+        }
+
         if (!client.application?.owner) await client.application?.fetch();
         if (!interaction.member.permissions.has(commandFile.permission)) {
           return await interaction.reply(`<:NotLikeThis:859024566860120064> **${interaction.member.user.username}**, vous n'avez pas la permission d'utiliser cette commande !`)
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-module.exports = (message, client) => {
+module.exports = async (message, client) => {
+
   const prefix = '.';
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -15,6 +16,19 @@ module.exports = (message, client) => {
       const commandName = file.replace('.js', '');
       const commandFile = require(`${__dirname}/../commands/${file}`);
       if (command === commandName) {
+        if (
+          message.channel.id !== '854063459259645962' && // #commandes
+          message.channel.id !== '863805344622706709' && // #général-dev
+          message.channel.id !== '942391531774562364' && // #tests-netherbot
+          !message.author.bot //|| // faut pas que ce soit un bot
+          //!message.author.roles.cache.get('863736992106151966') // si c'est pas un développeur
+        ) {
+          await message.channel.send(
+            `Allons ${message.author}, fais-moi plaisir... Va exécuter cette commande dans <#854063459259645962> s'il-te-plait <:UwU:865554940334964757>`
+          ).then(m => setTimeout(() => m.delete(), 10000));
+          await message.delete();
+          return;
+        }
         if (!message.member.permissions.has(commandFile.permission)) {
           return await message.reply(`<:NotLikeThis:859024566860120064> **${message.author.username}**, vous n'avez pas la permission d'utiliser cette commande !`)
         }
