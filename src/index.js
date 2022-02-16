8102c14a89eba6729e87e442a09622d63dabd4f9
NetherMC <thelio.trinite@outlook.com>
2022-02-16 16:36:35 +0100

    intents

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const { Routes } = require('discord-api-types/v9');
 require('dotenv').config();
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
+  intents: [
+    Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_VOICE_STATES
+  ]
 });
 
 fs.readdir(`${__dirname}/events`, (err, events) => {
